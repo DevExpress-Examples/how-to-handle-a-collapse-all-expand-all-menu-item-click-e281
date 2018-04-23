3e06922fc2c92ab2bd775a2cd390635c62173786
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,8 @@
+# How to handle a Collapse All/Expand All menu item click
+
+
+<p>The PivotGrid doesn't provide any events to be fired if the end-user clicks the Collapse All or Expand All menu item. You should derive from the PivotGridControl to implement these events.</p>
+
+<br/>
+
+
---
+++ VB/Form1.vb
@@ -0,0 +1,100 @@
+Imports Microsoft.VisualBasic
+Imports System
+Imports System.Collections.Generic
+Imports System.ComponentModel
+Imports System.Data
+Imports System.Drawing
+Imports System.Text
+Imports System.Windows.Forms
+Imports DevExpress.XtraPivotGrid
+Imports DevExpress.XtraPivotGrid.Data
+
+Namespace Q109817
+	Partial Public Class Form1
+		Inherits Form
+		Public Sub New()
+			InitializeComponent()
+
+			Dim pivot As New PivotGridControlEx()
+			pivot.Parent = Me
+			pivot.Dock = DockStyle.Fill
+			AddHandler pivot.FieldExpandedAll, AddressOf pivot_FieldExpandedAll
+
+			pivot.Fields.Add("group", PivotArea.RowArea)
+			pivot.Fields.Add("product", PivotArea.RowArea)
+			pivot.Fields.Add("sales", PivotArea.DataArea)
+
+			Dim table As New DataTable()
+			table.Columns.Add("group", GetType(String))
+			table.Columns.Add("product", GetType(String))
+			table.Columns.Add("sales", GetType(Integer))
+			table.Rows.Add("group 1", "product 1", 1)
+			table.Rows.Add("group 1", "product 2", 2)
+			table.Rows.Add("group 1", "product 3", 3)
+			table.Rows.Add("group 2", "product 4", 4)
+			table.Rows.Add("group 2", "product 5", 5)
+			table.Rows.Add("group 2", "product 6", 6)
+			pivot.DataSource = table.DefaultView
+		End Sub
+
+		Private Sub pivot_FieldExpandedAll(ByVal sender As Object, ByVal e As FieldExpandedEventArgs)
+			If e.Expanded Then
+				MessageBox.Show("The " & e.Field.FieldName & " field was " & ("expanded"))
+			Else
+				MessageBox.Show("The " & e.Field.FieldName & " field was " & ("collapsed"))
+			End If
+		End Sub
+	End Class
+
+	Public Class PivotGridControlEx
+		Inherits PivotGridControl
+		Protected Overrides Function CreateData() As PivotGridViewInfoData
+			Return New PivotGridViewInfoDataEx(Me)
+		End Function
+
+		Public Event FieldExpandedAll As EventHandler(Of FieldExpandedEventArgs)
+		Friend Sub RaiseFieldExpandedAll(ByVal field As PivotGridFieldBase, ByVal expanded As Boolean)
+			RaiseEvent FieldExpandedAll(Me, New FieldExpandedEventArgs(CType(field, PivotGridField), expanded))
+		End Sub
+	End Class
+
+	Public Class PivotGridViewInfoDataEx
+		Inherits PivotGridViewInfoData
+		Public Sub New(ByVal owner As PivotGridControlEx)
+			MyBase.New(owner)
+		End Sub
+
+		Public Shadows ReadOnly Property PivotGrid() As PivotGridControlEx
+			Get
+				Return CType(MyBase.PivotGrid, PivotGridControlEx)
+			End Get
+		End Property
+
+		Public Overrides Sub ChangeFieldExpanded(ByVal field As PivotGridFieldBase, ByVal expanded As Boolean)
+			MyBase.ChangeFieldExpanded(field, expanded)
+			PivotGrid.RaiseFieldExpandedAll(field, expanded)
+		End Sub
+	End Class
+
+	Public Class FieldExpandedEventArgs
+		Inherits EventArgs
+		Private field_Renamed As PivotGridField
+		Private expanded_Renamed As Boolean
+
+		Public Sub New(ByVal field As PivotGridField, ByVal expanded As Boolean)
+			Me.field_Renamed = field
+			Me.expanded_Renamed = expanded
+		End Sub
+
+		Public ReadOnly Property Field() As PivotGridField
+			Get
+				Return field_Renamed
+			End Get
+		End Property
+		Public ReadOnly Property Expanded() As Boolean
+			Get
+				Return expanded_Renamed
+			End Get
+		End Property
+	End Class
+End Namespace
